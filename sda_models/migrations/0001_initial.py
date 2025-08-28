ed5b54d310b4769f1758064aca260e46bb109bf0
13ahruz <bahruzshabili@gmail.com>
2025-08-28 11:30:14 +0400

    Reset migrations - create clean initial migration without file fields

---
+++ sda_models/migrations/0001_initial.py
@@ -0,0 +1,17 @@
+# Empty initial migration since we're working with existing tables
+# Created by FastAPI/SQLAlchemy
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        # This is an empty migration since our tables already exist
+        # The models are defined to match the existing database schema
+    ]
--- sda_models/migrations/__init__.py
+++ sda_models/migrations/__init__.py
@@ -1,1 +1,1 @@
-# Empty migrations __init__.py file
+# This file makes Python treat the directory as a package
